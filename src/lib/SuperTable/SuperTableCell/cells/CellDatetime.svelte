5cf437461c502903d8e99677e7895bdc1d044291
poirazis <poirazis@gmail.com>
2023-12-09 06:22:52 +0200

    wip

--- src/lib/SuperTable/SuperTableCell/cells/CellDatetime.svelte
+++ src/lib/SuperTable/SuperTableCell/cells/CellDatetime.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createEventDispatcher } from 'svelte'
-  import DatePicker from "../../../../../node_modules/@budibase/bbui/src/Form/Core/DatePicker.svelte"
+  import { DateInput } from 'date-picker-svelte'
+  import Popover from '../../../../../node_modules/@budibase/bbui/src/Popover/Popover.svelte'
 
 
 
@@ -13,7 +14,8 @@
   const dispatch = createEventDispatcher()
 
   let anchor 
-  let pickerStatus = "Closed"
+  let innerDate
+  let visible
 
   $: inEdit = $cellState == "Editing"
   $: if ( inEdit && anchor ) anchor.focus();
@@ -28,45 +30,34 @@
   tabindex="0"
   style:padding-left={cellOptions?.padding}
   style:padding-right={cellOptions?.padding}
-  on:blur={ () => pickerStatus == "Closed" ? cellState.lostFocus() : null }
 >
   {#if inEdit}
-    <div class="pickerWrapper">
-      <DatePicker 
-        {value} 
-        on:open={ (e) => pickerStatus = "Open" }
-        on:close={ (e) => { pickerStatus = "Closed"; } }
-        on:change={(e) => dispatch("change", e.detail ) } />
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="pickerWrapper" on:mousedown|preventDefault|stopPropagation={ () => visible = !visible }>
+      { formattedValue || value || "" }
     </div>
   {:else}
     <div class="inline-value"> 
       { formattedValue || value || "" }
     </div>
   {/if}
+
+  <Popover
+    {anchor}
+    dismissible
+    customHeight={"400px"}
+    open={visible}
+    on:close={ () => visible = false }
+    >
+      <DateInput bind:value={innerDate} visible />
+  </Popover>
 </div>
 
 <style>
-
   .pickerWrapper {
     flex: auto;
     display: flex;
     align-items: center;
-  }
-
-  :global(.pickerWrapper > .spectrum-Datepicker > .spectrum-Textfield > .spectrum-Textfield-input ) {
-    background-color: unset;
-    border: unset;
-    padding: unset;
-  }
-  :global(.pickerWrapper > .spectrum-Datepicker > .spectrum-Textfield ) {
-    background-color: unset;
-    border: unset;
-    padding: unset;
-  }
-  :global(.pickerWrapper > .spectrum-Datepicker > .spectrum-Picker ) {
-    background: unset;
-    border: unset;
-    padding: unset;
   }
   .inline-value { 
     flex-grow: 1;
